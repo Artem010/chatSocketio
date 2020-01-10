a28b3cb2ec7adebe619c04f7b35ad7e249b8cd40
artmiptv <cmert.123@yandex.ru>
2020-01-10 22:42:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require ('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(3000, "127.0.0.1");
+
+app.get('/', function(requset, respons) {
+  respons.sendFile(__dirname + '/index.html');
+});
+
+msgs = [];
+connections = [];
+
+io.sockets.on('connection', function(socket) {
+    console.log("Successful connection");
+    connections.push(socket);
+
+    socket.on('disconnect', function(data) {
+      connections.splice(connections.indexOf(socket), 1);
+      console.log("Successful disconnection");
+    });
+
+    socket.on('sendMsg', function(data) {
+      console.log(data);
+      msgs.push(data);
+      io.sockets.emit('addMsg', {msg:data.msg, name:data.name, color:data.color});
+    });
+
+    socket.on('pushFiveMsgs', function(data) {
+      socket.emit('PFM', msgs)
+    });
+});
